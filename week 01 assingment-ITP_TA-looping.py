eecdcf353956c03ebbf2d17608803b0de5330170
alexandersetiadi01 <43334689+alexandersetiadi01@users.noreply.github.com>
2018-09-24 14:55:04 +0700

    Update week 01 assingment-ITP_TA-looping.py

--- week 01 assingment-ITP_TA-looping.py
+++ week 01 assingment-ITP_TA-looping.py
@@ -1,54 +1,48 @@
 #right triangle
-m = 10
-for x in range(0, m):
-    print(" ")
-    for y in range(0, x+1):
-        print("*", end=" ")
+m = 10                          # the number of column that you want to make
+for x in range(0, m):           # a loop for the column
+    print(" ")                  # create space 
+    for y in range(0, x+1):     # a loop for the row    
+        print("*", end=" ")     # create the star with "*" until the loop stop  
+        
+print("")                      
 
-print("")
 
 #left triangle
-num = 10
-x = 1
-while x <= num:
-    print(" "*(num-x)+"*"*x)
-    x +=1
+num = 10                        # the number of column that you want to make
+x = 1                           # starting row and column 
+while x <= num:                 
+    print(" "*(num-x)+"*"*x)    # create space to make it go down or new row (num-x) times and for each row, create star("*") x times    
+    x +=1                       # each times it loop, increase the value of x by 1
 
 
-#pyramid
-num = 10
+#pyramid                        
+num = 10                        
 x = 1
 while x <= num:
-    print(" "*(num-x)+"*"*(x+(x-1)))
-    x +=1
+    print(" "*(num-x)+"*"*(x+(x-1)))    #create space to make it go down or new row (num-x) times and for each row, create star("*") (x+(x-1)) times
+    x +=1                               #each times it loop, increase the value of x by 1
 
 
 #diamond
 num = 10
 x = 1
 while x <= num:
-    print(" "*(num-x) + "*"*(2*x-1))
-    x +=1
+    print(" "*(num-x) + "*"*(2*x-1))    #create space to make it go down or new row (num-x) times and for each row, create star (2*x-1) times
+    x +=1                               #for each loop, increase the value of x by 1
 
-
-#reverse pyramid
-num = 10
-x = 1
-while x <= num:
-    print(" "*x + "*"*((num-x)*2-1))
-    x +=1
 
 
 #reverse left triangle
-num = 10
+num = 10                                
 x = 1
 while x <= num:
-    print(" "*x + "*"*(num-x))
-    x +=1
+    print(" "*x + "*"*(num-x))          #create space to make it go down or make row x times and for each row, create star (num-x) times
+    x +=1                               #for each loop, increase the value of x by 1
 
 #reverse right triangle
-num = 10
+num = 10        
 x = 1
 while x <= num:
-    print(" "*num + "*"*(num-x))
-    x +=1
+    print(" "*num + "*"*(num-x))        #create space to make it go down or make row (num) times and for each row, create star (num-x) times
+    x +=1                               #for each loop, increase the value of x by 1
