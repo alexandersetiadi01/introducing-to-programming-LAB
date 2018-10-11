069375fc1a3a98f8b9c6184c5b18e7a9cd659cd6
alexandersetiadi01 <43334689+alexandersetiadi01@users.noreply.github.com>
2018-10-11 15:15:16 +0700

    Add files via upload

---
+++ toll_2.py
@@ -0,0 +1,112 @@
+# Toll ver.2 by Ellery alexander setiadi
+
+class Car:  # make a class for car
+
+    def __init__(self, car):  # initialize car in class car
+        self.c = car  # declare that car is part of the class
+
+    def get_car(self):  # function for return car
+        return self.c  # return car
+
+
+class Truck:  # make a class for truck
+
+    def __init__(self, truck):  # initialize truck in class truck
+        self.t = truck  # declare that truck is part of the class
+
+    def get_truck(self):  # function for return truck
+        return self.t  # return truck
+
+
+class Bus:  # make a class for bus
+
+    def __init__(self, bus):  # initialize bus in class bus
+        self.b = bus  # declare that bus is part of the class
+
+    def get_bus(self):  # function for return bus
+        return self.b  # return bus
+
+
+class TollGate(Car, Bus, Truck):  # make a class for toll gate that inherit from car , bus and truck classes
+
+    def __init__(self, type):  # initialize type
+        super().__init__(type)  # make a super class
+
+    def car_fee(self):  # function for car's fee
+        Car.get_car = 6000  # car's fee
+        return Car.get_car  # return the car's fee
+
+    def bus_fee(self):  # function for bus's fee
+        Bus.get_bus = 8000  # bus's fee
+        return Bus.get_bus  # return the bus's fee
+
+    def truck_fee(self):  # function for truck's fee
+        Truck.get_truck = 10000  # truck's free
+        return Truck.get_truck  # return the trucks's fee
+
+
+restart = "Y"  # make a variable call restart
+
+
+def toll_operator():  # function for toll operator
+    car = 0        # the number of car start from 0
+    bus = 0        # the number of bus start from 0
+    truck = 0      # the number of truck start from 0
+    while restart != ("N"):  # using while loop which has condition restart is not "N"
+        print("|=========<Toll>=========|")  # decoration
+        print("category of vehicle: ")  # decoration
+        print("1. Car (Rp.6.000) \n2. Bus (Rp.8.000) \n3. Truck (Rp.10.000)")  # show the option
+        choice = input("type of vehicle: ").lower()  # to input the choice that you want
+        TG = TollGate(choice)  # make an object called TG for class TollGate
+        if choice == "car":  # if choice is equal to "car" , run the code
+            print("Fee: Rp.{}".format(TG.car_fee()))  # print car's fee from class
+            car += 1    # add the number of car by 1
+            menu = input("Is there any other vehicle (Y/N)?\n  ").upper()  # make a variable called menu to choose
+            # Y to continue the program and N to quit
+
+            if menu == "N":  # if menu is equal to "N"
+                print("car  bus  truck")    # decoration
+                print("{}     {}     {}".format(car, bus, truck))   # print the total number of car,bus,truck
+                revenue = (TG.car_fee() * car + TG.bus_fee() * bus + TG.truck_fee() * truck)
+                # calculate the total fee from all car, truck, bus
+                print("revenue = {}".format(revenue))   # print the revenue(total fee)
+
+                print("<exit program>\n have a nice trip")  # print this text
+
+                break  # stop the program
+
+        elif choice == "bus":  # if choice is equal to bus, run the code
+            print("Fee: Rp.{}".format(TG.bus_fee()))  # print bus's fee from class
+            bus += 1    # add the number of bus by 1
+            menu = input("Is there any other vehicle (Y/N)?\n ").upper()  # make a variable called menu to choose
+            # Y to continue the program and N to quit
+
+            if menu == "N":  # if menu is equal to N
+                print("car  bus  truck")  # decoration
+                print("{}     {}     {}".format(car, bus, truck))  # print the total number of car,bus,truck
+                revenue = (TG.car_fee() * car + TG.bus_fee() * bus + TG.truck_fee() * truck)
+                # calculate the total fee from all car, truck, bus
+                print("revenue = {}".format(revenue))  # print the revenue(total fee)
+
+                print("<exit program> \n have a nice trip")  # show this text
+                break  # stop the program
+
+        elif choice == "truck":  # if choice is equal to truck, run the code
+            print("Fee: Rp.{}".format(TG.truck_fee()))  # print truck's fee from class
+            truck += 1  # add the number of truck by 1
+            menu = input("Is there any other vehicle (Y/N)?\n ").upper()  # make a variable called menu to choose
+            # Y to continue the program and N to quit
+
+            if menu == "N":  # if menu is equal to N
+                print("car  bus  truck")  # decoration
+                print("{}     {}     {}".format(car, bus, truck))  # print the total number of car,bus,truck
+                revenue = (TG.car_fee() * car + TG.bus_fee() * bus + TG.truck_fee() * truck)
+                # calculate the total fee from all car, truck, bus
+                print("revenue = {}".format(revenue))  # print the revenue(total fee)
+
+                print("<exit program> \n have a nice trip")  # show this text
+                break  # stop the program
+
+
+toll_operator()  # call the function toll_operator
+
