f8d727ff912dc321da4a340a29e5fe7dd2d02f71
alexandersetiadi01 <43334689+alexandersetiadi01@users.noreply.github.com>
2018-10-15 19:44:19 +0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -46,12 +46,12 @@ def receptionist():                 # function for receptionist
     single = []                     # empty array for single
     double = []                     # empty array for double
     elite = []                      # empty array for elite
-    available_s = 15                # set available for single room to 15
-    available_d = 13                # set available for double room to 13
-    available_e = 8                 # set available for elite room to 8
+    available_s = 5                # set available for single room to 5
+    available_d = 3                # set available for double room to 3
+    available_e = 2                 # set available for elite room to 2
 
     while restart != ("N"):
-        print("1.check in\n2.check out")
+        print("1. check in\n2. check out\n3. reservation list")
         choice = input("=> ")       # type your choice
         if choice == "1":           # if choice is equal to 1, run the code
             print("available room: single = {} double = {} elite = {} ".format(available_s, available_d, available_e))
@@ -60,12 +60,12 @@ def receptionist():                 # function for receptionist
             category = input("category: ")  # choose your room category
             h = Hotel(category)             # create variable h to call class hotel
             if category == "1":             # if category is equal to 1, run the code
-                if len(single) <= 15:       # len single is less or equal to 15
+                if len(single) <= 5:       # len single is less or equal to 5
                     time = int(input("how long: ")) # input your time
-                    print(h.single_price() * time)  # print single room price times time
+                   print("Rp.{}".format(h.single_price() * time))  # print single room price times time
                     room_number = input("what is your reservation name:")   # enter your reservation name
                     single.append(room_number)      # add it to single
-                    available_s -= 1                # decrease available s by 1
+                    available_s -= 1                # decrease available s by 1        
                     menu = input("Y to continue the program/N to quit = ").upper() # choose what to do with the program
                     if menu == "N": # if menu is N, run the code
                         print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
@@ -77,9 +77,9 @@ def receptionist():                 # function for receptionist
                     print("no more room for single")
 
             if category == "2":
-                if len(double) <= 13:
+                if len(double) <= 3:
                     time = int(input("how long: "))  # input your time
-                    print(h.double_price() * time)   # print double room price times time
+                    print("Rp.{}".format(h.double_price() * time))  # print double room price times time
                     room_number = input("what is your reservation name: ")  # enter your reservation name
                     double.append(room_number)      # add it to double
                     available_d -= 1                # decrease available d decrease by 1
@@ -94,9 +94,9 @@ def receptionist():                 # function for receptionist
                     print("no more room for single")
 
             if category == "3":                 # if category is 3, run the code
-                if len(elite) <= 8:             # if len elite is less or equal to 8, run the code
+                if len(elite) <= 2:             # if len elite is less or equal to 8, run the code
                     time = int(input("how long: "))  # input your time
-                    print(h.elite_price() * time)   # print elite room price times time
+                    print("Rp.{}".format(h.single_price() * time))  # print elite room price times time
                     room_number = input("what is your reservation name:")   # enter your reservation name
                     elite.append(room_number)       # add it to elite
                     available_e -= 1                # available e is decreased by 1
@@ -111,7 +111,7 @@ def receptionist():                 # function for receptionist
                 else:
                     print("no more room for single")
 
-        if choice == "2":   # if choice is equal to 2, run the code
+        elif choice == "2":   # if choice is equal to 2, run the code
             print("available room: single = {} double = {} elite = {} ".format(available_s, available_d, available_e))
             print("room in use: single = {} double = {} elite = {}".format(len(single), len(double), len(elite)))
             print("category: 1.single 2.double 3.elite")
@@ -120,12 +120,7 @@ def receptionist():                 # function for receptionist
                 room_number = input("what is your reservation name:")   # enter your reservation name
                 if room_number in list(single): # if room_number is in list single, run this code
                     single.remove(room_number)  # remove it from single
-                    available_s += 1            # increase available s by 1
-                    print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
-                                                                                       available_e))
-                    print(
-                        "room in use: single = {} double = {} elite = {}".format(single, double, elite))
-
+                    available_s += 1            # increase available s by 1             
                     menu = input("Y to continue the program/N to quit = ").upper()  # choose what to do with the program
                     if menu == "N":
                         print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
@@ -138,10 +133,6 @@ def receptionist():                 # function for receptionist
                 if room_number in list(double):  # if room_number is in list double, run this code
                     double.remove(room_number)   # remove it form double
                     available_d += 1             # increase available d by 1
-                    print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
-                                                                                       available_e))
-                    print(
-                        "room in use: single = {} double = {} elite = {}".format(single, double, elite))
                     menu = input("Y to continue the program/N to quit = ").upper()  # choose what to do with the program
                     if menu == "N":
                         print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
@@ -154,10 +145,6 @@ def receptionist():                 # function for receptionist
                 if room_number in list(elite):  # if room_number is in list elite, run this code
                     elite.remove(room_number)   # remove it from elite
                     available_e += 1            # increase available e by 1
-                    print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
-                                                                                       available_e))
-                    print(
-                        "room in use: single = {} double = {} elite = {}".format(single, double, elite))
                     menu = input("Y to continue the program/N to quit = ").upper()  # choose what to do with the program
                     if menu == "N":
                         print("available room: single = {} double = {} elite = {} ".format(available_s, available_d,
@@ -165,7 +152,12 @@ def receptionist():                 # function for receptionist
                         print("room in use: single = {} double = {} elite = {}".format(len(single), len(double),
                                                                                        len(elite)))
                         break
-
+            
+          elif choice == "3":
+            print("reservation list:")
+            print("single: {}".format(single))      # get the entire list of single room
+            print("double: {}".format(double))      # get the entire list of double room
+            print("elite: {}".format(elite))        # get the entire list of elite room
 
 receptionist()          # call function receptionist
 
